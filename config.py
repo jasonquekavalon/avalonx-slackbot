93567fb3aab76b27b4111cdab0c23a408c0ed5bf
nishcafe <46063774+nishcafe@users.noreply.github.com>
2019-06-20 10:24:04 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-SLACK_BOT_TOKEN = "xoxb-something-something-YOUR_TOKEN_HERE"
-SLACK_VERIFICATION_TOKEN = "your-slack-signing-secret-here"
+import os
+
+SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
+SLACK_VERIFICATION_TOKEN = os.getenv("SLACK_VERIFICATION_TOKEN")
