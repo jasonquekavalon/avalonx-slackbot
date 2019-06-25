330b99014d715f56a5b30d1fcbc3e02bab48e812
Elias <elias.djurfeldt@avalonsolutions.com>
2019-06-25 10:32:15 +0200

    Updated cloudbuild and form data

--- app.py
+++ app.py
@@ -9,15 +9,18 @@ SLACK_VERIFICATION_TOKEN = cfg.SLACK_VERIFICATION_TOKEN
 app = Flask(__name__)
 
 # Create the datastore client
-ds_client=datastore_client.create_client("alfred-dev-1")
+ds_client = datastore_client.create_client("alfred-dev-1")
 
 # TODO: Add checks for all responses from slack api calls
+
+
 def verify_slack_token(request_token):
     """This should be used for ALL requests in the future"""
     if SLACK_VERIFICATION_TOKEN != request_token:
         print("Error: invalid verification token!")
         print("Received {} but was expecting {}".format(request_token, SLACK_VERIFICATION_TOKEN))
         return make_response("Request contains invalid Slack verification token", 403)
+
 
 @app.route("/slack/validation", methods=["POST"])
 def msg_validation(req):
@@ -26,24 +29,23 @@ def msg_validation(req):
 
 @app.route("/slack/gcp_support", methods=["POST"])
 def slack_gcp():
-    
-    
+
     # Save the message to the database using the datastore client
-    #request status field (pending)
-    
-    req = request.json
-    req["status"] = "Pending" 
+    # request status field (pending)
+
+    req = request.form.to_dict()
+    req["status"] = "Pending"
     message_id = datastore_client.add_item(ds_client, "message", req)
 
-    
     # send channel a response
     if (msg_validation(req)):
         slack_client.chat_postMessage(channel=req["channel_name"], text=req['message'])
         slack_client.chat_postMessage(channel=req["backend_channel"], text=req['message'])
-        return make_response("Your message id is " + str(message_id) + ". To check the status of your message, type '/status'.", 200) #response is giving me the wrong message_id
+        return make_response("Your message id is " + str(message_id) + ". To check the status of your message, type '/status'.", 200)  # response is giving me the wrong message_id
     else:
         return make_response("You're missing the required properties", 400)
-    
+
+
 @app.route("/response", methods=["PUT"])
 def slack_response():
     req = request.json
@@ -66,7 +68,7 @@ def slack_get():
 
 @app.route("/get/status", methods=["GET"])
 def slack_status():
-    
+
     status_query = request.args.get("message_id")
     status = datastore_client.get_status(ds_client, "message", status_query)
     return make_response(str(status), 200)
