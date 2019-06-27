1bfe3b00ce9de7ff684a40809e2b56c6d4513721
nishcafe <46063774+nishcafe@users.noreply.github.com>
2019-06-27 11:02:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -94,8 +94,9 @@ def slack_resolve_message():
     message_id = req['text'].split()[0]
     updated_status = "Completed"
     datastore_client.update_status(ds_client, "message", updated_status, message_id)
-
-    return make_response(f"Thank you for using the Alfred slack bot. We hope you have a nice day!", 200)
+    
+    slack_client.chat_postMessage(channel="alfred-dev-internal", text="Thank you for using the Alfred slack bot. We hope you have a nice day!")
+    return make_response("", 200)
 
 @app.route("/hello", methods=["POST"])
 def slash_hello():
