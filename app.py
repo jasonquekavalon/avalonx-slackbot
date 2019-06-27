ed9cb362f158820381f776e9b4dd05790156a7a4
Kennethcxj <kenneth.chia@avalonsolutions.com>
2019-06-27 13:25:16 +0800

    new updates

--- app.py
+++ app.py
@@ -43,14 +43,14 @@ def slack_gcp():
     req["status"] = "Pending"
     message_id = datastore_client.add_item(ds_client, "message", req)
 
-    response = f"*{req['user_name']}* from workspace *{req['team_domain']}* has a question in {req['channel_name']}: *{req['text']}*. To respond, type `/avalonx-respond {message_id} <response>`."
-
+    internal_message = f"*{req['user_name']}* from workspace *{req['team_domain']}* has a question in {req['channel_name']}: *{req['text']}*. To respond, type `/avalonx-respond {message_id} <response>`."
+    message = f"*{req['user_name']}* from workspace *{req['team_domain']}* says: *{req['text']}*"
     
     # send channel a response
     if (msg_validation(req)):
         # slack_client.chat_postMessage(channel=req["channel_name"], text=req['message'])
-        slack_client.chat_postMessage(channel=DEFAULT_BACKEND_CHANNEL, text=response)
-        slack_client.chat_postMessage(channel=CUSTOMER_CHANNEL, text=response)
+        slack_client.chat_postMessage(channel=DEFAULT_BACKEND_CHANNEL, text=internal_message)
+        slack_client.chat_postMessage(channel=CUSTOMER_CHANNEL, text=message)
         return make_response(f"Your message id is {message_id}. To check the status of your message, type `/avalonx-message-status {message_id}`.", 200)  
     else:
         return make_response("You're missing the required properties", 400)
