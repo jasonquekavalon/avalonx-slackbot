462372ccf815b1c8fea76dfd9ba433c61bff1f44
Kennethcxj <kenneth.chia@avalonsolutions.com>
2019-07-03 11:21:52 +0800

    3rd July updates

--- app.py
+++ app.py
@@ -39,39 +39,43 @@ def slack_gcp():
     # Save the message to the database using the datastore client
 
     req = request.form.to_dict()
-    print(req)
-    
-    req["status"] = "Pending"
+    query = ds_client.query(kind = 'message')
+    query.add_filter('team_domain', "=", req['team_domain'])
+    count = len(list(query.fetch())) + 1
 
+    req["status"] = "Pending"
+    friendly_id = f"{req['team_domain']}-{count}"    
+    req["status"] = "Pending"
+    req['friendly_id'] = friendly_id
     # send channel a response
     if (msg_validation(req)):
         
         if "message_id" not in req['text']:
             
             message = f"*{req['user_name']}* from workspace *{req['team_domain']}* says: *{req['text']}*. "
-            message_id = datastore_client.add_item(ds_client, "message", req)
+            friendly_id = datastore_client.add_item(ds_client, "message", req, friendly_id)
             
-            internal_message = f"*{req['user_name']}* from workspace *{req['team_domain']}* has a question in {req['channel_name']}: *{req['text']}*. To respond, type `/avalonx-respond {message_id} <response>`."
+            internal_message = f"*{req['user_name']}* from workspace *{req['team_domain']}* has a question in {req['channel_name']}: *{req['text']}*. To respond, type `/avalonx-respond {friendly_id} <response>`."
             slack_client.chat_postMessage(channel=DEFAULT_BACKEND_CHANNEL, text=internal_message)
         else:
-            message_id = req['text'].split()[1] #/avalonx message_id 1283219837857402 <message>
+            friendly_id = req['text'].split()[1] #/avalonx message_id 1283219837857402 <message>
             following_message_split = req["text"].split(maxsplit=2)[2:]
             following_message = following_message_split[0]
             
             message = f"*{req['user_name']}* from workspace *{req['team_domain']}* says: *{following_message}*. "
             
-            stored_messages = datastore_client.get_saved_messages(ds_client, "message", message_id)
+            stored_messages = datastore_client.get_saved_messages(ds_client, "message", friendly_id)
             if isinstance(stored_messages, str):
                 stored_messages = [stored_messages]
             stored_messages.append(following_message)           
             
-            datastore_client.update_message(ds_client, "message", stored_messages, message_id)
+            datastore_client.update_message(ds_client, "message", stored_messages, friendly_id)
                 
-            internal_message = f"*{req['user_name']}* from workspace *{req['team_domain']}* has a question in {req['channel_name']}: *{following_message}*. To respond, type `/avalonx-respond {message_id} <response>`."
+            internal_message = f"*{req['user_name']}* from workspace *{req['team_domain']}* has a question in {req['channel_name']}: *{following_message}*. To respond, type `/avalonx-respond {friendly_id} <response>`."
             slack_client.chat_postMessage(channel=DEFAULT_BACKEND_CHANNEL, text=internal_message)
         # slack_client.chat_postMessage(channel=req["channel_name"], text=req['message'])
         
-        return make_response(message + f"Your Message ID is {message_id}. To check the status of your message, type `/avalonx-message-status {message_id}`.", 200)   
+        return make_response(message + f"Your Message ID is {friendly_id}. To check the status of your message, type `/avalonx-message-status {friendly_id}`.", 200)   
     else:
         return make_response("You're missing the required properties", 400)
 
@@ -79,26 +83,26 @@ def slack_gcp():
 def slack_response():
     req = request.form.to_dict()
     
-    message_id = req['text'].split()[0]  # Should be a uuid if it was sent in as the first word
+    friendly_id = req['text'].split()[0]  # Should be a uuid if it was sent in as the first word
     # Ensure that message_id is a real uuid.
-    try:
-        _ = UUID(str(message_id), version=4)
-    except ValueError:
-        # If it's a value error, then the string 
-        # is not a valid hex code for a UUID.
-        return make_response("You're missing the required properties. Response should be in this format `/avalonx-respond <message id> <response>`. ", 400)
+    # try:
+    #     _ = UUID(str(message_id), version=4)
+    # except ValueError:
+    #     # If it's a value error, then the string 
+    #     # is not a valid hex code for a UUID.
+    #     return make_response("You're missing the required properties. Response should be in this format `/avalonx-respond <message id> <response>`. ", 400)
     response_to_message_split = req["text"].split(maxsplit=1)[1:]
     response_to_message = response_to_message_split[0]
-    channel_name = datastore_client.get_channelname(ds_client, "message", message_id)
-    response = f"*{req['user_name']}* from workspace *{req['team_domain']}* has responded to Message ID *{message_id}* in {req['channel_name']}: *{response_to_message}*. To respond, type `/avalonx message_id {message_id} <INPUT RESPONSE HERE>`. To resolve this conversation, type `/avalonx-resolve {message_id}`."
-    stored_responses = datastore_client.get_saved_responses(ds_client, "message", message_id)
+    channel_name = datastore_client.get_channelname(ds_client, "message", friendly_id)
+    response = f"*{req['user_name']}* from workspace *{req['team_domain']}* has responded to Message ID *{friendly_id}* in {req['channel_name']}: *{response_to_message}*. To respond, type `/avalonx message_id {friendly_id} <INPUT RESPONSE HERE>`. To resolve this conversation, type `/avalonx-resolve {friendly_id}`."
+    stored_responses = datastore_client.get_saved_responses(ds_client, "message", friendly_id)
     if stored_responses == None:
         stored_responses = []
         
     elif isinstance(stored_responses,str):
         stored_responses = [stored_responses]
     stored_responses.append(response_to_message)
-    datastore_client.update_response(ds_client, "message", stored_responses, message_id)
+    datastore_client.update_response(ds_client, "message", stored_responses, friendly_id)
     slack_client.chat_postMessage(channel=channel_name, text=response)
     return make_response("Response has been sent!", 200)
 
@@ -112,18 +116,18 @@ def slack_get():
 @app.route("/status", methods=["POST"])
 def slack_status():
     req = request.form.to_dict()
-    ticket_id = req['text']
-    status = datastore_client.get_status(ds_client, "message", ticket_id)
-    return make_response(f"Your status for ticket with ID = {ticket_id} is *{status}*", 200)
+    friendly_id = req['text']
+    status = datastore_client.get_status(ds_client, "message", friendly_id)
+    return make_response(f"Your status for ticket with ID = {friendly_id} is *{status}*", 200)
 
 @app.route("/resolve_message", methods=["POST"])
 def slack_resolve_message():
     req = request.form.to_dict()
-    message_id = req['text'].split()[0]
+    friendly_id = req['text'].split()[0]
     updated_status = "Completed"
-    datastore_client.update_status(ds_client, "message", updated_status, message_id)
+    datastore_client.update_status(ds_client, "message", updated_status, friendly_id)
   
-    slack_client.chat_postMessage(channel=DEFAULT_BACKEND_CHANNEL, text=f"*{req['user_name']}* from workspace *{req['team_domain']}* has resolved their ticket with Message ID *{message_id}*")
+    slack_client.chat_postMessage(channel=DEFAULT_BACKEND_CHANNEL, text=f"*{req['user_name']}* from workspace *{req['team_domain']}* has resolved their ticket with Message ID *{friendly_id}*")
     return make_response("Your issue has been resolved. Thank you for using the Alfred slack bot. We hope you have a nice day!", 200)
 
 @app.route("/hello", methods=["POST"])
