21df40a8da8b88f6265069f6439e9b9d1f7632ec
nishcafe <46063774+nishcafe@users.noreply.github.com>
2019-07-31 10:45:37 +0700

    Update

--- pubsub.py
+++ pubsub.py
@@ -4,3 +4,16 @@ from google.cloud import pubsub_v1
 project_id = "afred-dev-1"
 subscription_name = "projects/alfred-dev-1/subscriptions/file-upload"
 
+subscriber = pubsub_v1.SubscriberClient()
+subscription_path = subscriber.subscription_path(project_id, subscription_name)
+
+def callback(message):
+    print("Message recieved: {}".format(message))
+    message.ack()
+
+subscriber.subscribe(subscription_path, callback=callback)
+
+print("Listening for messages on {}".format(subscription_path))
+while True:
+    time.sleep(60)
+
