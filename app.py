4308c32d3c038120f1e32e308e7b06396549be6f
nishcafe <46063774+nishcafe@users.noreply.github.com>
2019-08-06 14:40:46 +0700

    Update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from uuid import UUID
 from threading import Thread
 import time
 
-from flask import Flask, request, make_response, Response
+from flask import Flask, request, make_response, Response, jsonify
 import requests
 from slack import WebClient
 from google.cloud import pubsub_v1, storage
@@ -223,8 +223,46 @@ def slack_buttons():
     else:
         slack_status()
     '''
-    print(req)
-
+    d = {
+    "text": "Would you like to play a game?",
+    "attachments": [
+        {
+            "text": "Choose a game to play",
+            "fallback": "You are unable to choose a game",
+            "callback_id": "wopr_game",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "game",
+                    "text": "Chess",
+                    "type": "button",
+                    "value": "chess"
+                },
+                {
+                    "name": "game",
+                    "text": "Falken's Maze",
+                    "type": "button",
+                    "value": "maze"
+                },
+                {
+                    "name": "game",
+                    "text": "Thermonuclear War",
+                    "style": "danger",
+                    "type": "button",
+                    "value": "war",
+                    "confirm": {
+                        "title": "Are you sure?",
+                        "text": "Wouldn't you prefer a good game of chess?",
+                        "ok_text": "Yes",
+                        "dismiss_text": "No"
+                    }
+                }
+            ]
+        }
+    ]
+    }
+    return make_response(jsonify(d), 200)
 
 def list_blobs_with_prefix(bucket_name, prefix):
     """Lists all the blobs in the bucket that begin with the prefix.
