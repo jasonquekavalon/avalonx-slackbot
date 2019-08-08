89b999b3f9dcf932c0546475f4e73f64e24b9fac
nishcafe <46063774+nishcafe@users.noreply.github.com>
2019-08-08 17:12:24 +0700

    Update

--- app.py
+++ app.py
@@ -190,6 +190,7 @@ def slack_response():
                 }
             ]
         }
+        logger.info("hi i made it here")
         slack_client.chat_postMessage(channel=channel_name, text=jsonify(msg))
     
     thread = Thread(target=process, kwargs={'req': req})  # Create background thread
